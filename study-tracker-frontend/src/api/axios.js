6a9c9b17f220c0d0cfcb45d3fba1eb564de0662e
Shivaranjani1711 <your_github_email@gmail.com>
2026-07-20 21:06:55 +0530

    Update API URL for production2

--- study-tracker-frontend/src/api/axios.js
+++ study-tracker-frontend/src/api/axios.js
@@ -1,13 +1,9 @@
 import axios from 'axios';
+
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL|| 'https://progressiq.onrender.com',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
 });
-
-
-// const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL || 'https://progressiq.onrender.com',
-//   // 'http://localhost:5000/api',
-// });
 
 // Attach the JWT to every request if we have one stored
 api.interceptors.request.use((config) => {
